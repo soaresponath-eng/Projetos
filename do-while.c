36b401ac93a04a18beda1f0a6d9dd26c8a300f64
Rosiane Soares Ponath <soaresponath@gmail.com>
2025-09-30 18:36:19 -0300

    commit incersao

---
+++ do-while.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int i = 1; //variavel de incremento
+
+    do {
+        printf("%d\n", i); //saida
+        i++; //incremento
+    } while (i <= 5); //condição
+
+
+
+
+
+    return 0;
+}
