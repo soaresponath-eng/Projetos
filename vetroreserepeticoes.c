7f6c2f5cb7365fd911d46f3201ca5c406d8875cc
Rosiane Soares Ponath <soaresponath@gmail.com>
2025-10-05 13:38:49 -0300

    novos 3

--- vetroreserepeticoes.c
+++ vetroreserepeticoes.c
@@ -1,52 +1,19 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+   int vetor[5]; // Declaração de um vetor de 5 inteiros
 
-char linha[10] = {'A','B','C','D','E','F','G','H','I','J'};
-int tabuleiro[10][10];
-int i, j;
-
-// Inicialização do tabuleiro com 0
-for(i = 0; i < 10; i++) {
-   for(j = 0; j < 10; j++) {
-   tabuleiro[i][j] = 0;
-    }
+   //Inicialização do vetor usando uma estrutura de repetição 'for'
+   for(int i = 0; i < 5; i++){
+      vetor[i] = i * 2; //Atribuindo valores ao vetor
    }
 
-// Definição dos navios (tamanho 3)
-int navio_horizontal[3] = {3, 3, 3};
-int navio_vertical[3] = {3, 3, 3};
+   //Exibindo os elementos do vetor usando estruturas de repetição 'for'
 
-// Coordenadas iniciais dos navios
-int linha_h = 2; // linha do navio horizontal
-int coluna_h = 4; // coluna inicial do navio horizontal
+   for(int i = 0; i < 5; i++){
+      printf("vetor[%d] = %d\n", i, vetor[i]);
+   }
 
-int linha_v = 5; // linha inicial do navio vertical
-int coluna_v = 7; // coluna do navio vertical
-// Posicição do navio horizontal no tabuleiro
- for(i = 0; i < 3; i++) {
-   tabuleiro[linha_h][coluna_h + i] = navio_horizontal[i];
-}
-
-// Posicição do navio vertical no tabuleiro
- for(i = 0; i < 3; i++) {
- tabuleiro[linha_v + i][coluna_v] = navio_vertical[i];
-}
-
-// Imprimir números das colunas no topo
-  printf("  "); // Espaço para alinhar com as letras das linhas
-for(j = 0; j < 10; j++) {
-   printf("%2d ", j+1);
-}
-printf("\n");
-
-// Exibe o tabuleiro com letras e números
- for(i = 0; i < 10; i++) {
- printf("%c ", linha[i]);
-  for(j = 0; j < 10; j++) {
-   printf("%2d ", tabuleiro[i][j]);
-}
-printf("\n");
-}
    return 0;
+
 }
